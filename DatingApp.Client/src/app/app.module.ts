12991ac4ed21f1283474d4703a6e6e75300cc6e7
Edwidge Amisial <edwidge.amisial@gmail.com>
2022-09-30 14:51:16 -0400

    End of Section 6

--- DatingApp.Client/src/app/app.module.ts
+++ DatingApp.Client/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ListsComponent } from './lists/lists.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
+import { SharedModule } from './modules/shared.module';
 import { NavComponent } from './nav/nav.component';
 import { RegisterComponent } from './register/register.component';
 
@@ -15,16 +19,20 @@ import { RegisterComponent } from './register/register.component';
   declarations: [
     AppComponent,
     HomeComponent,
+    ListsComponent,
+    MemberDetailComponent,
+    MemberListComponent,
+    MessagesComponent,
     NavComponent,
     RegisterComponent
   ],
   imports: [
     AppRoutingModule,
     BrowserAnimationsModule,
-    BsDropdownModule.forRoot(),
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- DatingApp.Client/src/app/nav/nav.component.ts
+++ DatingApp.Client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 import { AccountService } from '../services/account.service';
 
@@ -10,7 +12,7 @@ import { AccountService } from '../services/account.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) { }
 
   ngOnInit(): void {
 
@@ -21,13 +23,17 @@ export class NavComponent implements OnInit {
       .login(this.model)
       .subscribe({
         next: (response) => {
-          console.log(response);
+          this.router.navigateByUrl('/members');
         },
-        error: (e) => console.log(e)
+        error: (e) => {
+          console.log(e);
+          this.toastr.error(e.error);
+        }
       });
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
